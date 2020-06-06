a009b11942c2011fe231fd0e7c9e2a204e1f0839
Sumit Sinha <51187812+sumit07sinha@users.noreply.github.com>
2020-06-06 21:54:36 +0530

    Update app.js

--- src/server/app.js
+++ src/server/app.js
@@ -3,7 +3,6 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var cors = require("cors");
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(
